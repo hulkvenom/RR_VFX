8ea3cc2b27d3dcc5a2f2435580dfb8629e08576c
hulkvenom <115841729+hulkvenom@users.noreply.github.com>
2023-11-22 22:59:24 -0600

    anims

---
+++ RR_VFX/Assets/_MyFiles/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Transform magicSpawn;
+    public GameObject magicAttackPrefab;
+    public float magicMoveSpeed;
+    private Animator player_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            UseMagic();
+        }
+    }
+
+    public void UseMagic()
+    {
+        player_Animator.SetTrigger("Attack");
+    }
+
+    public void ShootMagic()
+    {
+        GameObject magicAttackClone = Instantiate(magicAttackPrefab, magicSpawn.transform.position, magicSpawn.transform.rotation);
+        Rigidbody rbody = magicAttackClone.GetComponent<Rigidbody>();
+        rbody.AddForce(magicAttackClone.transform.forward * magicMoveSpeed, ForceMode.Impulse);
+    }
+
+}
